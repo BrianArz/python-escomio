dc2605e3edd6c1ff0ecc17b99a2c0f8959f38504
Arz Dev <arz.dev@outlook.com>
2024-03-18 13:17:08 -0600

    [app] + flask configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
+# Packages
 from flask import Flask
+import os
+
+# Local files
 from .auth import auth_routes as auth
 from .health import health_routes as health
 
 
-def create_app(config_name):
+def create_app():
     flask_app = Flask(__name__, instance_relative_config=True)
 
+    # Get environment, if not defined 'development' will be default
+    environment = os.getenv('FLASK_ENV', 'development')
+
     # App configuration
-    flask_app.config.from_pyfile('config.py', silent=True)
+    if environment == 'production':
+        flask_app.config.from_pyfile('production_config.py', silent=True)
+    else:
+        flask_app.config.from_pyfile('development_config.py', silent=True)
 
     # Blueprints registry
     flask_app.register_blueprint(auth.auth_bp, url_prefix='/auth')
