376fde7147fb8f92b9c79a0172ac638a48306019
Arz Dev <arz.dev@outlook.com>
2024-03-18 07:46:12 -0600

    [auth] + models

---
+++ app/auth/models/firebase_error.py
@@ -0,0 +1,24 @@
+# Packages
+from typing import List
+
+
+class ErrorElement:
+    domain: str
+    message: str
+    reason: str
+
+    def __init__(self, domain: str, message: str, reason: str) -> None:
+        self.domain = domain
+        self.message = message
+        self.reason = reason
+
+
+class FirebaseError:
+    code: int
+    errors: List[ErrorElement]
+    message: str
+
+    def __init__(self, code: int, errors: List[ErrorElement], message: str) -> None:
+        self.code = code
+        self.errors = errors
+        self.message = message
---
+++ app/auth/utils/parse_error.py
@@ -0,0 +1,25 @@
+# Packages
+import json
+
+# Local files
+from app.auth.models.firebase_error import FirebaseError
+
+
+def parse_error(error: str) -> FirebaseError:
+    """
+    Parses Firebase error string
+
+    :param error: Error string
+    :return: FirebaseError object
+    """
+    try:
+        error_json = json.loads(error)
+
+        error_code = error_json.get('error', {}).get('code')
+        error_details = error_json.get('error', {}).get('errors')
+        error_message = error_json.get('error', {}).get('message')
+
+        return FirebaseError(error_code, error_details, error_message)
+
+    except json.JSONDecodeError:
+        pass
