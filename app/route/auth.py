1ce84ecf2f462fc670761908694017aaa0ff33da
Omar Leyva <leo300920@hotmail.com>
2024-03-21 23:55:03 -0600

    [Auth/sing-up] + Create Accounts

--- app/route/auth.py
+++ app/route/auth.py
@@ -2,7 +2,6 @@
 from flask import Blueprint, jsonify, request
 from requests.exceptions import HTTPError
 
-
 # Auth files
 from app.core import parse_error, parse_sign_in
 from app.service import firebase_service
@@ -15,7 +14,7 @@ firebase_app = firebase_service.init_firebase()
 firebase_auth = firebase_app.auth()
 
 
-@auth_bp.route('/sign-in-with-email-and-password', methods=['GET'])
+@auth_bp.route('/sign-in-with-email-and-password', methods=['POST'])
 def login():
     """
     Signs into FirebaseAuth with user credentials
@@ -34,6 +33,33 @@ def login():
             )
         )
         return jsonify(response.__dict__), 200
+
+    except HTTPError as error:
+        error_data = parse_error(error.strerror)
+
+        if error_data is None:
+            return jsonify({'Internal server error': error.strerror}), 500
+
+        return jsonify({'message': error_data.message}), 400
+
+
+@auth_bp.route('/sign-up', methods=['POST'])
+def sign_up():
+    """
+    Creates a new user in FirebaseAuth with email and password
+
+    :parameter: User email and password
+    :return: SignInResponse object
+    """
+    # Get email and password from HTTP Request
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    try:
+        user = firebase_auth.create_user_with_email_and_password(email, password)
+        # If successful, let's also sign in the user and return the response
+        response = parse_sign_in(firebase_auth.sign_in_with_email_and_password(email, password))
+        return jsonify(response.__dict__), 201  # 201 indicates resource created
 
     except HTTPError as error:
         error_data = parse_error(error.strerror)
