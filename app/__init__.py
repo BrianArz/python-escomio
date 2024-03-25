88228627f5a561d9e1f194ddb61c06e88375f4ac
Arz Dev <arz.dev@outlook.com>
2024-03-24 20:23:57 -0600

    [app] + firebase_auth object singleton refactor

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,23 @@ import os
 
 # Local files
 from app.route import health_bp, auth_bp
+from .service.firebase import firebase_service
+
+firebase_app = None
+firebase_admin = None
+
+
+def create_firebase_app():
+    global firebase_app
+    if firebase_app is None:
+        firebase_app = firebase_service.init_firebase()
+    return firebase_app
+
+
+def create_firebase_admin():
+    global firebase_admin
+    if firebase_admin is None:
+        firebase_admin = firebase_service.init_firebase_admin()
 
 
 def create_app():
@@ -21,5 +38,11 @@ def create_app():
     # Blueprints registry
     flask_app.register_blueprint(auth_bp, url_prefix='/auth')
     flask_app.register_blueprint(health_bp, url_prefix='/health')
+
+    # Initializes firebase application
+    create_firebase_app()
+
+    # Initializes firebase admin app
+    create_firebase_admin()
 
     return flask_app
