886cef24b8fb1fab160e35f8f7783033dbc95d0e
Arz Dev <arz.dev@outlook.com>
2024-03-25 09:28:15 -0600

    [rasa/route] + obtains data from endpoint request

--- app/route/rasa.py
+++ app/route/rasa.py
@@ -1,38 +1,43 @@
 # Packages
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 import requests
 
 # Local files
 from app.core import authorize
 from app.core import parse_test_question
 import app.config as config
-from app.schema.rasa.test_question import TestQuestion
 
 # Defines the blueprint
 rasa_bp = Blueprint('rasa', __name__)
 
-test_question = TestQuestion('Sender1', 'cual es tu funcion?')
 
-
-@rasa_bp.route('/test-question', methods=['GET'])
-#@authorize
+@rasa_bp.route('/test-question', methods=['POST'])
+@authorize
 def test_question():
     """
     Asks simple question to rasa server
 
+    :parameter: User name and user question
     :return: Rasa server response
     """
+
+    # Get sender and message from HTTP Request
+    sender = request.json.get('sender')
+    message = request.json.get('message')
+
+    # Validates empty request body
+    if sender is None or message is None:
+        return jsonify({'message': 'Missing Information'}), 400
 
     api_url = f"{config.RASA_URI}:{config.RASA_PORT}/{config.TEST_QUESTION}"
 
     try:
-        response = requests.post(api_url, json=test_question.__dict__)
+        response = requests.post(api_url, json={'sender': sender, 'message': message})
         response_json = response.json()
 
-        first_response = response_json[0]
-
-        parsed_response = parse_test_question(first_response)
-        print(parsed_response.__dict__)
+        # Returns first response of request
+        question = parse_test_question(response_json[0])
+        return jsonify(question.__dict__), 200
 
     except Exception as error:
         return jsonify({'Internal server error': error}), 500
