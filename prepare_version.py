586fb0cd6891ae0d7141d192a6db4e92b2d4c394
Arz Dev <arz.dev@outlook.com>
2024-03-25 00:13:25 -0600

    [prepare_version.py] + fix

--- prepare_version.py
+++ prepare_version.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 
 
@@ -12,7 +11,7 @@ is_dirty = '1' if git_diff else '0'
 commit_hash = run_command("git rev-parse HEAD")[:6]
 
 version_info = f"""
-'1.0.{git_version}.{is_dirty}' - {commit_hash};
+1.0.{git_version}.{is_dirty} - {commit_hash}
 """
 
 file_path = "version_info.txt"
