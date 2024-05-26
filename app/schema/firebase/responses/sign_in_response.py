f2c475dfae7ad55cd77ee98b247f335a12aac079
Arz Dev <arz.dev@outlook.com>
2024-05-26 09:44:59 -0600

    [firebase/responses/sign_in] + uid

--- app/schema/firebase/responses/sign_in_response.py
+++ app/schema/firebase/responses/sign_in_response.py
@@ -2,8 +2,10 @@ class FirebaseSignInResponse:
     expires_in: str
     refresh_token: str
     id_token: str
+    uid: str
 
-    def __init__(self, expires_in: str, refresh_token: str, id_token: str) -> None:
+    def __init__(self, expires_in: str, refresh_token: str, id_token: str, uid: str) -> None:
         self.expires_in = expires_in
         self.refresh_token = refresh_token
         self.id_token = id_token
+        self.uid = uid
