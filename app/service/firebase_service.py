a707b79e70a4b06c1fc453a32014fdff7913113e
Arz Dev <arz.dev@outlook.com>
2024-05-26 08:41:32 -0600

    [service/firebase] + refactor

--- app/service/firebase_service.py
+++ app/service/firebase_service.py
@@ -1,52 +1,104 @@
 # Packages
 import json
+from flask import jsonify, current_app
+from requests.exceptions import HTTPError
 import pyrebase
 import firebase_admin
 from firebase_admin import credentials
+
+from app.core import FirebaseParser
+from app.schema import FirebaseCredentialsRequest
 
 
 class FirebaseService:
     @classmethod
     def init_app(cls, app):
         cls.app = app
-        cls.firebase_app = cls.init_firebase()
-        cls.firebase_admin = cls.init_firebase_admin()
+        cls.firebase_app = cls._init_firebase()
+        cls.firebase_admin = cls._init_firebase_admin()
 
     @staticmethod
-    def init_firebase():
+    def _init_firebase():
         """
         Initializes pyrebase application with configuration file
-
-        :return: pyrebase app
         """
         try:
             with open('instance/firebase_config.json', 'r') as config_file:
                 config = json.load(config_file)
+
             return pyrebase.initialize_app(config)
-        except json.JSONDecodeError as e:
-            raise ValueError("Invalid JSON configuration for Firebase.") from e
+
+        except json.JSONDecodeError as json_ex:
+            current_app.logger.error(f"Invalid JSON configuration for Firebase: {json_ex}")
+            raise ValueError("Invalid JSON configuration for Firebase.") from json_ex
 
     @staticmethod
-    def init_firebase_admin():
+    def _init_firebase_admin():
         """
         Initializes firebase sdk admin app
-
-        :return: Firebase sdk admin app
         """
         try:
             cred = credentials.Certificate("instance/firebase_admin_key.json")
             return firebase_admin.initialize_app(cred)
-        except FileNotFoundError:
+
+        except FileNotFoundError as file_ex:
+            current_app.logger.error(f"Firebase admin credential file not found: {file_ex}")
             raise FileNotFoundError("Firebase admin credentials file not found.")
 
     @staticmethod
     def get_firebase_auth():
         """
         Gets firebase sdk admin auth application
-
-        :return: Firebase sdk admin auth application
         """
         if hasattr(FirebaseService, 'firebase_app'):
             return FirebaseService.firebase_app.auth()
+
         else:
-            raise Exception("Firebase app is not initialized.")
+            current_app.logger.error("Unable to get firebase auth app")
+            raise Exception("Unable to get firebase auth app")
+
+    @classmethod
+    def sign_in(cls, creds: FirebaseCredentialsRequest):
+        try:
+            firebase_auth = cls.get_firebase_auth()
+
+            response = firebase_auth.sign_in_with_email_and_password(creds.email, creds.password)
+            fb_response = FirebaseParser.parse_sign_in(response)
+
+            return jsonify(fb_response.__dict__), 200
+
+        except HTTPError as http_ex:
+            current_app.logger.error(f"Firebase API sign-in error response has occurred {http_ex}")
+
+            error_response = FirebaseParser.parse_error(http_ex.strerror)
+            if error_response is None:
+                return jsonify({"message": "Internal Server Error"}), 500
+
+            return jsonify({"message": error_response.message}), 500
+
+        except Exception as ex:
+            current_app.logger.error(f"Unable to sign in to firebase {ex}")
+            return jsonify({"message": "Unable to sign in to firebase"}), 500
+
+    @classmethod
+    def sign_up(cls, creds: FirebaseCredentialsRequest):
+        try:
+            firebase_auth = cls.get_firebase_auth()
+
+            response = firebase_auth.create_user_with_email_and_password(creds.email, creds.password)
+            fb_response = FirebaseParser.parse_sign_in(response)
+
+            return jsonify(fb_response.__dict__), 200
+
+        except HTTPError as http_error:
+            current_app.logger.error(f"Firebase API sign-up error response has occurred {http_error}")
+
+            error_response = FirebaseParser.parse_error(http_error.strerror)
+            if error_response is None:
+                return jsonify({"message": "Internal Server Error"}), 500
+
+            return jsonify({"message": error_response.message}), 500
+
+        except Exception as ex:
+            current_app.logger.error(f"Unable to sign up to firebase {ex}")
+            return jsonify({"message": "Unable to sign up to firebase"}), 500
