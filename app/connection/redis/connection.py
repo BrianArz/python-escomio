d1df3d6217aab3a2c694081c10def8624886f65a
Arz Dev <arz.dev@outlook.com>
2024-05-26 17:57:24 -0600

    [connection/redis] + fix

--- app/connection/redis/connection.py
+++ app/connection/redis/connection.py
@@ -3,19 +3,19 @@ from flask import current_app
 
 
 class RedisConnection:
-    redis_connection = None
+    _redis_connection = None
 
     @classmethod
     def init_connection(cls):
-        if cls.redis_connection is None:
+        if cls._redis_connection is None:
             redis_uri = current_app.config.get('REDIS_URI', 'localhost')
             redis_port = current_app.config.get('REDIS_PORT', 6379)
             redis_url = f'{redis_uri}:{redis_port}'
-            cls.redis_connection = get_redis_connection(url=redis_url)
-        return cls.redis_connection
+            cls._redis_connection = get_redis_connection(url=redis_url)
+        return cls._redis_connection
 
     @classmethod
     def get_connection(cls):
-        if cls.redis_connection is None:
-            cls.redis_connection = cls.init_connection()
-        return cls.redis_connection
+        if cls._redis_connection is None:
+            cls.init_connection()
+        return cls._redis_connection
