501006da956129178c6aab3de53eb618b1d79418
Arz Dev <arz.dev@outlook.com>
2024-06-01 13:42:12 -0600

    [core/decorators/authorize] + redis cache refactor

--- app/core/decorators/authorize.py
+++ app/core/decorators/authorize.py
@@ -1,23 +1,30 @@
 from functools import wraps
-from flask import request, jsonify
+from flask import request, jsonify, current_app
 
-from firebase_admin import auth
+from app.respository import RedisRepository
 
 
 def authorize(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        bearer_token = request.headers.get('Authorization', None)
-        if bearer_token is None:
-            return jsonify({'message': 'Unauthorized'}), 401
 
-        # noinspection PyBroadException
+        """ This function obtains auth data sent through cookies from the client, if something fails we just assume
+        the request is not valid and does not come from a trustfully source """
+
         try:
-            token = bearer_token.replace('Bearer ', '')
-            decoded_token = auth.verify_id_token(token, clock_skew_seconds=0)
-            request.user = decoded_token
+            access_token = request.cookies.get('X-Access-Token', None)
+            uid = request.cookies.get('X-Uid', None)
 
-        except Exception as e:
+            if access_token is None or uid is None:
+                return jsonify({'message': 'Unauthorized request'}), 401
+
+            redis_user = RedisRepository.get_user_by_id(uid)
+
+            if redis_user is None or redis_user.id_token != access_token:
+                return jsonify({'message': 'Unauthorized request'}), 401
+
+        except Exception as ex:
+            current_app.logger.error(f"Unable to obtain auth cookies information: {ex}")
             return jsonify({'message': 'Unauthorized'}), 401
 
         return f(*args, **kwargs)
