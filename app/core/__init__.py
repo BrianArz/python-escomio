0976441da07eac732c9493941c71b2df138e9fae
Arz Dev <arz.dev@outlook.com>
2024-06-08 14:37:57 -0600

    [app/core/init] + imports

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,5 +1,6 @@
 from .utils.parser.firebase_parser import FirebaseParser
 from .utils.parser.rasa_parser import RasaParser
+from .utils.parser.firebase_error_view_parse import FirebaseErrorViewParse
 
 from .validators.endpoint_validators import EndpointValidators
 
