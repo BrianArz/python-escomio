e7b7cd62169f3db3b7c1bd1c5ddcd6df94f53efd
Arz Dev <arz.dev@outlook.com>
2024-06-09 19:17:37 -0600

    [instance/development] + mongo configuration

--- instance/development_config.py
+++ instance/development_config.py
@@ -4,5 +4,12 @@ RASA_PORT = '6005'
 REDIS_URI = "redis://localhost"
 REDIS_PORT = "6379"
 
+MONGO_URI = "localhost"
+MONGO_PORT = "27017"
+MONGO_DATABASE = "escomio"
+MONGO_USER = "root"
+MONGO_PASSWORD = "admin"
+MONGO_SOURCE = "admin"
+
 COOKIE_SECURE = False
 COOKIE_SAME_SITE = 'Strict'
