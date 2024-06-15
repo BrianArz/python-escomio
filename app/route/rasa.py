69ad73de31d78d884a0af045411eed46e3484e7e
Arz Dev <arz.dev@outlook.com>
2024-06-15 10:53:06 -0600

    [route/rasa/ask-question] + refactor - useless documentation

--- app/route/rasa.py
+++ app/route/rasa.py
@@ -6,12 +6,9 @@ from app.service import RasaService
 rasa_bp = Blueprint('rasa', __name__)
 
 
-@rasa_bp.route('/test-question', methods=['POST'])
+@rasa_bp.route('/ask-question', methods=['POST'])
 @authorize
 def test_question():
-    """
-    Asks simple question to rasa server
-    """
     information, error_response, status_code = EndpointValidators.validate_rasa_question(request)
 
     if error_response:
