dc1330a25d6441ca401998de4e56cb86f3c0139c
Arz Dev <arz.dev@outlook.com>
2024-06-15 17:34:55 -0600

    [route/rasa] + add message to conversation

--- app/route/rasa.py
+++ app/route/rasa.py
@@ -1,19 +1,31 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, make_response, request
 
-from app.core import authorize, EndpointValidators, ExecuteRequest
-from app.service import RasaService
+from app.core import authorize, EndpointValidators, ChatBo
 
 rasa_bp = Blueprint('rasa', __name__)
 
 
-@rasa_bp.route('/ask-question', methods=['POST'])
+@rasa_bp.route('/create-conversation', methods=['POST'])
 @authorize
-def test_question():
+def create_conversation():
     information, error_response, status_code = EndpointValidators.validate_rasa_question(request)
 
     if error_response:
         return error_response, status_code
 
-    return ExecuteRequest.execute(RasaService.ask_question, information)
+    response = ChatBo.start_chat(information)
+    return make_response(response)
+
+
+@rasa_bp.route('/add-message-to-conversation', methods=['POST'])
+@authorize
+def add_message_to_conversation():
+    information, error_response, status_code = EndpointValidators.validate_rasa_add_question(request)
+
+    if error_response:
+        return error_response, status_code
+
+    response = ChatBo.add_message_to_conversation(information)
+    return make_response(response)
 
 
