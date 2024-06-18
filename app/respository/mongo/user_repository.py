2f02882da22e3016bc68b257466e4d0e0b4181f5
Arz Dev <arz.dev@outlook.com>
2024-06-18 07:15:09 -0600

    [mongo/user] + get users

--- app/respository/mongo/user_repository.py
+++ app/respository/mongo/user_repository.py
@@ -15,3 +15,22 @@ class MongoUserRepository:
             return user
         except DoesNotExist:
             return None
+
+    @classmethod
+    def get_all_users(cls):
+        return MongoUser.objects.all()
+
+    @classmethod
+    def to_json(cls, user: MongoUser):
+        return {
+            "username": user.username,
+            "escom_id": user.escom_id,
+            "firebase_uid": user.firebase_uid,
+            "role": "Estudiante" if user.role == 1 else "Administrador",
+            "is_active": "Activo" if user.active else "Deshabilitado"
+        }
+
+    @classmethod
+    def all_users_to_json(cls):
+        users = cls.get_all_users()
+        return [cls.to_json(user) for user in users]
